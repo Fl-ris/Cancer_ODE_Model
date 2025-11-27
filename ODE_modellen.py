e1d08c500e19ed9217bab4df6283634f7c731a8a
Fl-ris <florismenninga@gmail.com>
2025-11-27 21:41:25 +0100

    Eerste opzet solver gemaakt.

--- ODE_modellen.py
+++ ODE_modellen.py
@@ -212,6 +212,63 @@ class tumorODE:
 
 
         return Ts, Vs
+    
+
+    def solve(self, method, model):
+        t_list = []
+        v_list = []
+        t = 0
+        y = 0
+        
+        if method == "Euler":
+            for _ in range(self.n):
+                # Update volgens Euler's methode:
+                dy = self.model(t, y)
+                t = t + self.delta_t
+                y = y + dy * self.delta_t
+
+                t_list.append(t)
+                v_list.append(y)
+
+        elif method == "Heun":
+            for _ in range(self.n):
+                # Eerste update:
+                dydt1 = self.model(t, y)
+                t1 = t + self.delta_t
+                y1 = y + dydt1 * self.delta_t
+                # Tweede update:
+                dydt2 = self.model(t1, y1)
+                t = t + self.delta_t
+                y = y + (dydt1 + dydt2) / 2.0 * self.delta_t
+               
+                t_list.append(t)
+                v_list.append(y)
+
+        elif method == "Runge-Kutta":
+            for _ in range(self.n):
+                # Eerste update:
+                dydt1 = self.model(t, y)
+                t1 = t + 0.5 * self.delta_t
+                y1 = y + 0.5 * dydt1 * self.delta_t
+                # Tweede update:
+                dydt2 = self.model(t1, y1)
+                t2 = t + 0.5 * self.delta_t
+                y2 = y + 0.5 * dydt2 * self.delta_t
+                # Derde update:
+                dydt3 = self.model(t2, y2)
+                t3 = t + self.delta_t
+                y3 = y + dydt2 * self.delta_t
+                # Vierde update:
+                dydt4 = self.model(t3, y3)
+                t = t + self.delta_t
+                y = y + (dydt1 + 2.0 * dydt2 + 2.0 * dydt3 + dydt4) / 6.0 * self.delta_t
+
+                t_list.append(t)
+                v_list.append(y)
+
+
+        else:
+            print("Onbekende solver.... probeer: 'Euler', 'Heun' of 'Runge-Kutta'.")
 
 
     def plot(Ts, Vs):
