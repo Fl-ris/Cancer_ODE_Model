aa090ef0b740a3104aef8e53ccfa85f6c6230939
Fl-ris <florismenninga@gmail.com>
2025-11-29 21:17:24 +0100

    De moddelen kunnen nu ook een vergelijking returnen en lists beginnen bij 1 i.p.v. 0.

--- ODE_modellen.py
+++ ODE_modellen.py
@@ -25,12 +25,14 @@ class tumorODE:
         return f"Start volume: {self.volume}, aantal dagen (n): {self.n}, tijdsstapgrootte: {self.delta_t}"
         
 
-    def linear_model(self, c):
-        Ts = []
-        Vs = []
-        t = 0
-
+    def linear_model(self, c, mode=None):
         # Dv/Dt = c
+        if mode == "equation":
+            return lambda t, v: c
+
+        Ts = [0]
+        Vs = [self.volume]
+        t = 0
 
         for _ in range(self.n):
             t = t + self.delta_t
@@ -44,39 +46,62 @@ class tumorODE:
         return Ts, Vs
 
 
-    def exponentieel_model(self, c):
-        Ts = []
-        Vs = []
-        t = 0
-
+    def exponentieel_model(self, c, mode=None):
         # Dv/Dt = c * V
+        if mode == "equation":
+            return lambda t, v: c * v
+
+        Ts = [0]
+        Vs = [self.volume]
+        t = 0
 
         for _ in range(self.n):
             t = t + self.delta_t
-
             delta_volume = c * self.volume * self.delta_t
-
             self.volume = delta_volume + self.volume
 
             Ts.append(t)
             Vs.append(self.volume)
 
-
         return Ts, Vs
 
 
-    def mendelsohn_model(self, c, d):
-        Ts = []
-        Vs = []
+    def mendelsohn_model(self, c, d, mode=None):
+        # Dv/Dt = c * V^d
+        if mode == "equation":
+            return lambda t, v: c * math.pow(v, d)
+
+        Ts = [0]
+        Vs = [self.volume]
         t = 0
 
-        # Dv/Dt = c * V^d
         # Werkt nog niet... math range error
 
         for _ in range(self.n):
             t = t + self.delta_t
+            delta_volume = c * math.pow(self.volume, d) * self.delta_t
+            self.volume = delta_volume + self.volume
 
-            delta_volume = c * math.pow(self.volume,d) * self.delta_t
+            Ts.append(t)
+            Vs.append(self.volume)
+
+
+        return Ts, Vs
+
+
+    def linear_gelimiteerdegroei_model(self, c, d, mode=None):
+        # Dv/Dt = c * V / (V + d)
+        if mode == "equation":
+            return lambda t, v: c * (v / (v + d))
+
+        Ts = [0]
+        Vs = [self.volume]
+        t = 0
+
+        for _ in range(self.n):
+            t = t + self.delta_t
+
+            delta_volume = c * (self.volume / (self.volume + d)) * self.delta_t
 
             self.volume = delta_volume + self.volume
 
@@ -87,51 +112,33 @@ class tumorODE:
         return Ts, Vs
 
 
-    def linear_gelimiteerdegroei_model(self, c, d):
-        Ts = []
-        Vs = []
-        t = 0
+    def oppervlak_gelimiteerdegroei_model(self, c, d, mode=None):
+        # Dv/Dt = c * V / V^d
+        if mode == "equation":
+            return lambda t, v: c * (v / (math.pow(v, d)))
 
-        # Dv/Dt = c * V / V + d
+        Ts = [0]
+        Vs = [self.volume]
+        t = 0
 
         for _ in range(self.n):
             t = t + self.delta_t
-
-            delta_volume = c * (self.volume / (self.volume +d)) * self.delta_t
-
-            self.volume = delta_volume + self.volume
-
-            Ts.append(t)
-            Vs.append(self.volume)
-
-
-        return Ts, Vs
-
-
-    def oppervlak_gelimiteerdegroei_model(self, c, d):
-        Ts = []
-        Vs = []
-        t = 0
-
-        # Dv/Dt = c * V/V+d
-
-        for _ in range(self.n):
-            t = t + self.delta_t
-
             delta_volume = c * (self.volume / (math.pow(self.volume,d))) * self.delta_t
-
             self.volume = delta_volume + self.volume
 
             Ts.append(t)
             Vs.append(self.volume)
-
 
         return Ts, Vs
     
 
-    def exponentieel_afvlakkendegroei_model(self, c, v_max):
-        Ts = []
-        Vs = []
+    def exponentieel_afvlakkendegroei_model(self, c, v_max, mode=None):
+        # Dv/Dt = c * (Vmax - V)
+        if mode == "equation":
+            return lambda t, v: c * (v_max - v)
+
+        Ts = [0]
+        Vs = [self.volume]
         t = 0
 
         # Dv/Dt = c * (Vmax - V)
@@ -150,40 +157,40 @@ class tumorODE:
         return Ts, Vs
 
 
-    def von_bertalanffy_model(self, c, d):
-        Ts = []
-        Vs = []
+    def von_bertalanffy_model(self, c, d, mode=None):
+        # Dv/Dt = c * V^2/3 - d * V
+        if mode == "equation":
+            return lambda t, v: c * math.pow(v, 2/3) - d * v
+
+        Ts = [0]
+        Vs = [self.volume]
         t = 0
 
-        # Dv/Dt = c * V^2/3 - d * V
         # Math domain error wanneer er een tijdsstapgrootte van 1 gebruikt wordt, werkt wel met 0.1.
 
         for _ in range(self.n):
             t = t + self.delta_t
-
-            delta_volume = c * math.pow(self.volume,2/3) - d * self.volume * self.delta_t
-
+            delta_volume = (c * math.pow(self.volume, 2/3) - d * self.volume) * self.delta_t
             self.volume = delta_volume + self.volume
 
             Ts.append(t)
             Vs.append(self.volume)
 
-
         return Ts, Vs
 
 
-    def allee_effect_groei(self, c, v_min, v_max):
-        Ts = []
-        Vs = []
-        t = 0
-
+    def allee_effect_groei(self, c, v_min, v_max, mode=None):
         # Dv/Dt = c * (V - Vmin) * (Vmax - V)
+        if mode == "equation":
+            return lambda t, v: c * (v - v_min) * (v_max - v)
+
+        Ts = [0]
+        Vs = [self.volume]
+        t = 0
 
         for _ in range(self.n):
             t = t + self.delta_t
-
             delta_volume = c * (self.volume - v_min) * (v_max - self.volume) * self.delta_t
-
             self.volume = delta_volume + self.volume
 
             Ts.append(t)
@@ -193,12 +200,15 @@ class tumorODE:
         return Ts, Vs
 
 
-    def gompertz_groei(self, c, v_max):
-        Ts = []
-        Vs = []
+    def gompertz_groei(self, c, v_max, mode=None):
+        # Dv/Dt = c * V * ln(Vmax / V)
+        if mode == "equation":
+            return lambda t, v: c * v * log(v_max / v)
+
+        Ts = [0]
+        Vs = [self.volume]
         t = 0
 
-        # Dv/Dt = c * V * ln(Vmax / V)
 
         for _ in range(self.n):
             t = t + self.delta_t
@@ -215,15 +225,15 @@ class tumorODE:
     
 
     def solve(self, method, model):
-        t_list = []
-        v_list = []
+        t_list = [0]
+        v_list = [self.volume]
         t = 0
-        y = 0
+        y = self.volume
         
         if method == "Euler":
             for _ in range(self.n):
                 # Update volgens Euler's methode:
-                dy = self.model(t, y)
+                dy = model(t, y)
                 t = t + self.delta_t
                 y = y + dy * self.delta_t
 
@@ -233,11 +243,11 @@ class tumorODE:
         elif method == "Heun":
             for _ in range(self.n):
                 # Eerste update:
-                dydt1 = self.model(t, y)
+                dydt1 = model(t, y)
                 t1 = t + self.delta_t
                 y1 = y + dydt1 * self.delta_t
                 # Tweede update:
-                dydt2 = self.model(t1, y1)
+                dydt2 = model(t1, y1)
                 t = t + self.delta_t
                 y = y + (dydt1 + dydt2) / 2.0 * self.delta_t
                
@@ -247,36 +257,41 @@ class tumorODE:
         elif method == "Runge-Kutta":
             for _ in range(self.n):
                 # Eerste update:
-                dydt1 = self.model(t, y)
+                dydt1 = model(t, y)
                 t1 = t + 0.5 * self.delta_t
                 y1 = y + 0.5 * dydt1 * self.delta_t
                 # Tweede update:
-                dydt2 = self.model(t1, y1)
+                dydt2 = model(t1, y1)
                 t2 = t + 0.5 * self.delta_t
                 y2 = y + 0.5 * dydt2 * self.delta_t
                 # Derde update:
-                dydt3 = self.model(t2, y2)
+                dydt3 = model(t2, y2)
                 t3 = t + self.delta_t
                 y3 = y + dydt2 * self.delta_t
                 # Vierde update:
-                dydt4 = self.model(t3, y3)
+                dydt4 = model(t3, y3)
                 t = t + self.delta_t
                 y = y + (dydt1 + 2.0 * dydt2 + 2.0 * dydt3 + dydt4) / 6.0 * self.delta_t
 
                 t_list.append(t)
                 v_list.append(y)
 
+    
 
         else:
             print("Onbekende solver.... probeer: 'Euler', 'Heun' of 'Runge-Kutta'.")
 
+        return t_list, v_list
 
-    def plot(Ts, Vs):
+    def plot(Ts, Vs, color, label):
         """
         Geef de lijst met tijdspunten en volume waarden die daar bijhoren om een grafiek te krijgen die deze plot. 
         """
         
         plt.figure(figsize=(12.8, 4.8))
-        plt.axvline(0.0, color='k')
-        plt.plot(Ts, Vs, label="test")
-        plt.show() 
+        plt.axvline(0.0, color=color)
+        plt.plot(Ts, Vs, label=label)
+        plt.legend()
+        plt.xlabel("Tijd")
+        plt.ylabel("Volume")
+        plt.show()
